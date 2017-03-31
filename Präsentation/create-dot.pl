499753ad6299c88c9879cff67af246946197a6f0
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-03-31 18:00:38 +0200

    2017-03-31

--- Präsentation/create-dot.pl
+++ Präsentation/create-dot.pl
@@ -5,7 +5,29 @@ use strict;
 open (my $dot, '>:encoding(utf-8)', 'ablauf.dot') or die;
 print $dot "digraph A {\n";
 
+my %htmls;
+
 for my $html (glob '*.html') {
+
+  if ($html eq 'index.html') {
+
+    open my $in, '<:encoding(utf-8)', $html or die;
+    while (my $line = <$in>) {
+
+      while ($line =~ s/<a href=["'](.*?)\.html["']//) {
+
+        my $to = $1;
+        $to = "S_$to";
+        $to =~ s/-/_/g;
+
+        $htmls{$to}{indexed} = 1;
+
+      }
+
+    }
+    close $in;
+    next;
+  }
 
   my ($html_) = $html =~ /(.*)\.html$/;
   $html_ = "S_$html_";
@@ -23,29 +45,55 @@ for my $html (glob '*.html') {
       $to = $1;
       $to = "S_$to";
       $to =~ s/-/_/g;
+
     }
   }
   close $in;
   
 
-
-  print $dot "$html_ [
-  shape=plaintext;
-  label=< <table>
-    <tr><td>$title</td></tr>
-    <tr><td>$html</td></tr>
-   </table> >];
-";
-
   if ($to) {
+    $htmls{$to}{title} = $title;
+  }
 
+#  print $dot "$html_ [
+#  shape=plaintext;
+#  label=< <table>
+#    <tr><td>$title</td></tr>
+#    <tr><td>$html</td></tr>
+#   </table> >];
+#";
+#
+  if ($to) {
     print $dot "$html_ -> $to;\n";
   }
 
 
 }
 
+for my $html_ (keys %htmls) {
+
+  my $color='';
+  if ($htmls{$html_}{indexed}) {
+    $color = "\n     color=red";
+  }
+
+  my $title = $htmls{$html_}{title} // '?';
+
+  my $shape = "  $html_ [
+  shape=plaintext $color
+   label=< <table>
+     <tr><td>$title</td></tr>
+     <tr><td>$html_</td></tr>";
+
+
+  $shape .= "
+    </table> >];
+";
+  print $dot $shape;
+}
+
 print $dot "}\n";
 close $dot;
 
-system "dot ablauf.dot -Tpdf -oablauf.pdf && op ablauf.pdf";
+# system "dot ablauf.dot -Tpdf -oablauf.pdf && op ablauf.pdf";
+system "dot ablauf.dot -Tpdf -oablauf.pdf";
